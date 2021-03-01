a7e9054f97ac83a88ce208c0180ddbebd43935a7
Fhelipe Rodrigues <fhelipe.rodrigues@fatec.sp.gov.br>
2021-03-01 15:44:20 -0300

    Exercicio 2 finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CursoComponent } from './curso/curso.component';
+import { ListaCursosComponent } from './lista-cursos/lista-cursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursoComponent,
+    ListaCursosComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/curso/curso.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curso',
+  templateUrl: './curso.component.html',
+  styleUrls: ['./curso.component.css']
+})
+export class CursoComponent{
+  nome = "POOA"
+  carga_horaria = 200
+}
